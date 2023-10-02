4e53eca3f8fafe7365014c5bd552a9ddc5d91a2e
leona0708 <yuli0363@uni.sydney.edu.au>
2023-10-02 23:51:05 +1100

    add profile page and avatar

---
+++ app/src/main/java/comp5216/sydney/edu/au/grocerylist/Profile.java
@@ -0,0 +1,15 @@
+package comp5216.sydney.edu.au.grocerylist;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Profile extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_profile);
+
+
+    }
+}
